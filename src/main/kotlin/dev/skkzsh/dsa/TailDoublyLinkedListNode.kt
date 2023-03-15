7d059c204b65f3cb199c7f1dac757573c22f726d
skkzsh <skkzsh12@gmail.com>
2023-03-16 00:45:22 +0900

    Implement DoublyLinkedList

---
+++ src/main/kotlin/dev/skkzsh/dsa/TailDoublyLinkedListNode.kt
@@ -0,0 +1,39 @@
+package dev.skkzsh.dsa
+
+class TailDoublyLinkedListNode(val value: Int) {
+    private var next: TailDoublyLinkedListNode? = null
+    private var prev: TailDoublyLinkedListNode? = null
+
+    // O(n)
+    fun append(new: TailDoublyLinkedListNode) {
+        var n = this
+        while (n.next != null) {
+            n = n.next!!
+        }
+
+        n.next = new
+        new.prev = n
+    }
+
+    fun delete(n: TailDoublyLinkedListNode) {
+        n.prev?.next = n.next
+        n.next?.prev = n.prev
+    }
+
+    fun at(num: Int): TailDoublyLinkedListNode {
+        var n = this
+        for (i in 0 until num) {
+            n = n.next!!
+        }
+        return n
+    }
+
+    fun print() {
+        var n = this
+        while (n.next != null) {
+            println(n.value)
+            n = n.next!!
+        }
+        println(n.value)
+    }
+}
---
+++ src/test/kotlin/dev/skkzsh/dsa/TailDoublyLinkedListNodeTest.kt
@@ -0,0 +1,38 @@
+package dev.skkzsh.dsa
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TailDoublyLinkedListNodeTest {
+
+    private val n = TailDoublyLinkedListNode(100)
+
+    @BeforeEach
+    fun setUp() {
+        n.append(TailDoublyLinkedListNode(101))
+        n.append(TailDoublyLinkedListNode(102))
+        n.append(TailDoublyLinkedListNode(103))
+
+        // n.print() // debug
+    }
+
+    @Test
+    fun append() {
+        assertEquals(100, n.at(0).value)
+        assertEquals(101, n.at(1).value)
+        assertEquals(102, n.at(2).value)
+        assertEquals(103, n.at(3).value)
+    }
+
+    @Test
+    fun delete() {
+        n.delete(n.at(2))
+
+        // n.print() // debug
+
+        assertEquals(100, n.at(0).value)
+        assertEquals(101, n.at(1).value)
+        assertEquals(103, n.at(2).value)
+    }
+}
