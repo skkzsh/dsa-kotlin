0a406ae0eeddd61c397354b7e041cd50ebebbe1d
skkzsh <skkzsh12@gmail.com>
2023-03-16 21:55:21 +0900

    gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.8.0"
 //    application
+    `maven-publish`
 }
 
 group = "dev.skkzsh"
@@ -25,3 +26,24 @@ kotlin {
 //application {
 //    mainClass.set("MainKt")
 //}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://gitlab.com/api/v4/projects/44282995/packages/maven")
+            name = "GitLab"
+            credentials(HttpHeaderCredentials::class) {
+                name = "Deploy-Token"
+                value = System.getenv("MAVEN_DEPLOY_TOKEN")
+            }
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+        }
+    }
+}
