13692e74e2fd0496d0d9bf4c2f2a005a2279cd7e
skkzsh <skkzsh12@gmail.com>
2023-03-17 22:16:36 +0900

    Add GPR credentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/skkzsh/dsa-kotlin")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
