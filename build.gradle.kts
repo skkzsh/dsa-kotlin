f78273866a5120c988358011317bea85c99d258b
skkzsh <skkzsh12@gmail.com>
2023-08-19 14:29:06 +0900

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.22"
 //    application
     `maven-publish`
 }
@@ -20,7 +20,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
 
 //application {
